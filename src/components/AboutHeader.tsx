174e77a82ae164e913528c3e603d63cf98729c60
Josh Weaver <joshweaver23@gmail.com>
2024-06-16 19:21:17 -0400

    tweaks

--- src/components/AboutHeader.tsx
+++ src/components/AboutHeader.tsx
@@ -2,7 +2,8 @@ function AboutHeader() {
   return (
     <header className="flex flex-row pb-12" id="about">
       <h1 className="text-6xl text-left max-w-lg">
-        Hi, I&apos;m Josh. <br /> Experienced Software Engineer based in CHS.
+        Hi, I&apos;m Josh. <br /> Experienced <br /> Full Stack <br /> Software
+        Engineer based in CHS.
       </h1>
     </header>
   )
