00a4ea3c03da8d3f49765d53bb57a1fa5de14a64
Josh Weaver <joshweaver23@gmail.com>
2024-06-18 14:44:49 -0400

    fix mobile menu toggle

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 const NavItems = [
   {
     title: 'About',
@@ -18,6 +20,8 @@ const NavItems = [
 ]
 
 function Navigation() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
   return (
     <nav className="bg-white fixed w-full z-20 top-0 start-0 border-b border-gray-200">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-end mx-auto p-4">
@@ -27,6 +31,7 @@ function Navigation() {
           className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
           aria-controls="navbar-default"
           aria-expanded="false"
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
         >
           <span className="sr-only">Open main menu</span>
           <svg
@@ -45,10 +50,17 @@ function Navigation() {
             />
           </svg>
         </button>
-        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+        <div
+          className={
+            isMenuOpen
+              ? 'w-full md:block md:w-auto'
+              : 'hidden w-full md:block md:w-auto'
+          }
+          id="navbar-default"
+        >
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white">
             {NavItems.map((item) => (
-              <li key={item.title}>
+              <li key={item.title} onClick={() => setIsMenuOpen(false)}>
                 <a
                   href={item.link}
                   className="block py-2 px-3 text-gray-500 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-gray-700 md:p-0 lowercase"
